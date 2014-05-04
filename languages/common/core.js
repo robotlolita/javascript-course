be89cf4bbc407e61bd662cacb78e2ccc3d86d0f4
Quildreen "Sorella" Motta <quildreen@gmail.com>
2014-05-04 02:23:18 -0300

    Adds a REPL.

--- languages/common/core.js
+++ languages/common/core.js
@@ -1,9 +1,17 @@
+var readline  = require('readline')
+var vm        = require('vm')
 var curry     = require('core.lambda').curry
 var escodegen = require('escodegen')
+
 
 exports.parse = curry(2, parse)
 function parse(parser, text) {
   return parser.matchAll(text, 'program')
+}
+
+exports.parseExpr = curry(2, parseExpr)
+function parseExpr(parser, text) {
+  return parser.matchAll(text, 'expr')
 }
 
 exports.compile = curry(2, compile)
@@ -14,4 +22,38 @@ function compile(compiler, ast) {
 exports.generate = generate
 function generate(ast) {
   return escodegen.generate(ast)
+}
+
+exports.repl = curry(2, repl)
+function repl(language, state) {
+  console.log('Using the language: ' + language.name)
+  console.log('Type :quit to exit (or ^D)')
+  console.log('')
+  loopEvaluation( language
+                , vm.createContext(state)
+                , readline.createInterface({ input: process.stdin
+                                           , output: process.stdout }))
+}
+
+function loopEvaluation(language, state, rl) {
+  rl.question(language.name + '> ', function(program) {
+    evaluateCommand(language, state, program)
+    loopEvaluation(language, state, rl)
+  })
+}
+
+function evaluateCommand(language, state, program) {
+  return program === ':quit'?  process.exit(0)
+  :      /* otherwise */       console.log(run(language, state, program))
+}
+
+function run(language, state, program) {
+  var ast  = parseExpr(language.Parser, program)
+  var code = generate(compile(language.Compiler, ast))
+
+  try {
+    return vm.runInNewContext(code, state, '<repl>')
+  } catch(e) {
+    console.log(e.stack)
+  }
 }
